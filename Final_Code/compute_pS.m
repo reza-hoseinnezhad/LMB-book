ff8a7ff14abe3d102ac4482d45ebe09003c07832
reza-hoseinnezhad <rezah@rmit.edu.au>
2024-08-08 11:39:12 +1000

    Delete Final_Code directory

--- Final_Code/compute_pS.m
+++
@@ -1,8 +0,0 @@
-function pS = compute_pS(X)
-
-if isempty(X)
-    pS= [];
-else
-    pS= 0.99*ones(size(X,1),1);
-    pS= pS(:);
-end
--- Final_Code/demo.m
+++
@@ -1,7 +0,0 @@
-Model = gen_model;
-Truth = gen_truth(Model);
-%plot_truth(Truth);
-Meas=  gen_meas(Model,Truth);
-[Estimates,LMBs] =   run_filter(Model,Meas);
-%plot_truth_and_particles(Truth,LMBs);
-GeneratePlots(Truth,Meas,Estimates);
